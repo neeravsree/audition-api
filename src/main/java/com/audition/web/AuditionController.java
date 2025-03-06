d7f426097cf52a70d6c78567c989c4edf1568c14
sreelekshmi <sree.sreelekshmi@gmail.com>
2025-03-07 09:25:01 +1100

    Openapi swagger changes

--- src/main/java/com/audition/web/AuditionController.java
+++ src/main/java/com/audition/web/AuditionController.java
@@ -3,6 +3,8 @@ package com.audition.web;
 import com.audition.model.AuditionPost;
 import com.audition.model.Comments;
 import com.audition.service.AuditionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,12 +18,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Tag(
+    name = "CRUD REST APIs for Audition API",
+    description = "CRUD REST APIs for Audition API"
+)
 @RestController
 @AllArgsConstructor
 public class AuditionController {
 
     private AuditionService auditionService;
 
+    @Operation(
+        summary = "Get API for returning all posts or filter based on userId",
+        description = "REST API for returning all posts or filter based on userId"
+    )
     @RequestMapping(value = "/posts", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<AuditionPost> getPosts(@RequestParam(required = false) Integer userId) {
 
@@ -32,6 +43,10 @@ public class AuditionController {
         return allPosts;
     }
 
+    @Operation(
+        summary = "Get API for returning all posts based on postId",
+        description = "REST API for returning all posts based on postId"
+    )
     @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getPosts(@PathVariable("id") final String postId) {
 
@@ -42,13 +57,20 @@ public class AuditionController {
         return ResponseEntity.ok(auditionPosts);
     }
 
-
+    @Operation(
+        summary = "Get API for returning all comments based on postId",
+        description = "REST API for returning all commnets based on postId"
+    )
     @GetMapping("/posts/{postId}/comments")
     public List<Comments> getComments(@PathVariable("postId") final String postId) {
 
         return auditionService.getComments(postId);
     }
 
+    @Operation(
+        summary = "Get API for returning all posts based on postId",
+        description = "REST API for returning all posts based on postId"
+    )
     @GetMapping("/posts/comments")
     public List<Comments> getCommentsForPost(@RequestParam("postId") final String postId) {
 
